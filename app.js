2d7db768297c2fdff3c949016eb2b3187dc66d7b
Änubhab <hota000anubhab@gmail.com>
2020-12-16 22:26:53 +0530

    Database functionality added
    Former-commit-id: 274eaed7ba7bf841d26d881ef8c2063f9ff524e0

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const morgan  = require('morgan');
 const mongoose = require('mongoose');
 var favicon = require('serve-favicon');
+const Booking = require(__dirname+'/models/booking');
 
 
 const app = express();
 
+const dbURI = "***REMOVED***"
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
 app.set('view engine','ejs');
 
-app.listen(3000);
+
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -29,6 +36,42 @@ app.get('/Home',(req,res)=>{
 
 app.get('/booking',(req,res)=>{
     res.render('booking');
+})
+
+app.get('/Admin',(req,res)=>{
+    Booking.find().sort({ createdAt: -1})
+    .then(result=>{
+        res.render('Admin',{booking: result});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+    
+})
+
+app.get('/admin/:id',(req,res)=>{
+    const id = req.params.id;
+    Booking.updateOne({_id:id},{status:1})
+    .then(result=>{
+        res.redirect('/Admin')
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+})
+
+app.post('/booking',(req,res)=>{
+    const booking = new Booking(req.body);
+    console.log(booking.check_in);
+    console.log(booking.check_out);
+    booking.status = 0;
+    booking.save()
+        .then(result=>{
+            res.redirect('/Home');
+        })
+        .catch(err=>{
+            console.log(err);
+        })
 })
 
 app.use((req,res)=>
