f439dc1b2e646f1373ea5610f72f20cf0069be48
prabhavtewari <64471223+prabhavtewari@users.noreply.github.com>
2020-12-19 20:02:56 +0530

    Update app.js
    Former-commit-id: e9cf9b3e3cac82304fce4a99eb843db02914f598

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const Booking = require(__dirname+'/models/booking');
 
 
 const app = express();
+app.listen(3000);
 
 const dbURI = "***REMOVED***"
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(3000))
+  .then(result)
   .catch(err => console.log(err));
 
 app.set('view engine','ejs');
