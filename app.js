5cbb6d073c3ba47dcef7f563c8d5a6f9d6a22000
Prabhav <prabhavtewari@gmail.com>
2020-12-20 00:54:33 +0530

    heroku changes and all
    Former-commit-id: e2f5d9f73c1c77963cc6a4b21c99e30b7681496b

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const Booking = require(__dirname+'/models/booking');
 
 
 const app = express();
-app.listen(3000);
+
+const port = process.env.PORT || 3000;
 
 const dbURI = "***REMOVED***"
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result)
+.then(result => {app.listen(port, () => {
+    console.log(`Our app is running on port ${ port }`);
+});})
   .catch(err => console.log(err));
 
 app.set('view engine','ejs');
