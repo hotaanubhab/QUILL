22ae1b8b1a34a16321485f6f9fcdd8a3960eb322
Prabhav <prabhavtewari@gmail.com>
2020-12-20 20:59:48 +0530

    partial masonry
    Former-commit-id: f40c467899f2ae108d840df9f13bb69642af9952

---
+++ public/app1.js
@@ -0,0 +1,8 @@
+import Masonry from'masonry-layout';
+window.onload = () => {
+    const grid = document.querySelector('.grid');
+    const masonry = new Masonry(grid,{
+        itemSelector:'.grid-item',
+    });
+
+}
