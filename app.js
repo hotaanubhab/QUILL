faddb1d83ae7bfd25f9f754740e40bca3af82034
Änubhab <hota000anubhab@gmail.com>
2020-12-30 14:23:26 +0530

    authentication added
    Former-commit-id: 37f61fbb460d3f572c8b44c9c1a6dab678354138

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const morgan  = require('morgan');
 const mongoose = require('mongoose');
 var favicon = require('serve-favicon');
 const Booking = require(__dirname+'/models/booking');
+const User = require(__dirname+'/models/User');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 
 const app = express();
@@ -19,20 +23,96 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set('view engine','ejs');
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { uname: '', password: '' };
+  
+    // incorrect uname
+    if (err.message === 'incorrect username') {
+      errors.uname = 'That username is not registered';
+    }
+  
+    // incorrect password
+    if (err.message === 'incorrect password') {
+      errors.password = 'That password is incorrect';
+    }
+  
+    // duplicate email error
+    if (err.code === 11000) {
+      errors.uname = 'that username is already registered';
+      return errors;
+    }
+  
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+      // console.log(err);
+      Object.values(err.errors).forEach(({ properties }) => {
+        // console.log(val);
+        // console.log(properties);
+        errors[properties.path] = properties.message;
+      });
+    }
+}
 
+    const createToken = (id) => {
+        return jwt.sign({ id }, '***REMOVED***');
+      };
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(favicon(__dirname + '/public/content/favicon.ico'));
 app.use(morgan('dev'));
+app.use(cookieParser());
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
+app.get('*', checkUser);
+
 app.get('/',(req,res)=>{
     res.redirect('/Home')
 });
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+});
+
+app.get('/signup',(req,res)=>{
+    res.render('signup')
+});
+
+app.post('/signup',async (req, res) => {
+    const { uname, password } = req.body;
+  
+    try {
+      const user = await User.create({ uname, password });
+      res.status(201).json({ user: user._id });
+    }
+    catch(err) {
+      const errors = handleErrors(err);
+      res.status(400).json({ errors });
+    }
+   
+  }
+)
+
+app.post('/login',async (req,res)=>{
+    const { uname, password } = req.body;
+
+  try {
+    const user = await User.login(uname, password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true});
+    res.status(200).json({ user: user._id });
+  } 
+  catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+
+})
 
 app.get('/Home',(req,res)=>{
     res.render('index');
@@ -50,7 +130,7 @@ app.get('/confirm',(req,res)=>{
     res.render('confirm')
 })
 
-app.get('/Admin',(req,res)=>{
+app.get('/Admin',requireAuth,(req,res)=>{
     Booking.find().sort({ createdAt: -1})
     .then(result=>{
         res.render('Admin',{booking: result});
@@ -61,7 +141,9 @@ app.get('/Admin',(req,res)=>{
     
 })
 
-app.get('/admin/:id&:s',(req,res)=>{
+
+
+app.get('/admin/:id&:s',requireAuth,(req,res)=>{
     const id = req.params.id;
     const s = req.params.s;
     Booking.updateOne({_id:id},{status:s})
@@ -71,6 +153,17 @@ app.get('/admin/:id&:s',(req,res)=>{
     .catch(err=>{
         console.log(err);
     });
+})
+
+app.get('/admin/:id',requireAuth,(req,res)=>{
+    const id = req.params.id;
+    Booking.findOne({_id:id})
+    .then(result=>{
+        res.render('Details',{booking: result});
+    })
+    .catch(err=>{
+        console.log(err);
+    })
 })
 
 app.post('/booking',(req,res)=>{
@@ -86,7 +179,7 @@ app.post('/booking',(req,res)=>{
         })
 })
 
-app.delete('/admin/:id',(req,res)=>{
+app.delete('/admin/:id',requireAuth,(req,res)=>{
     console.log("delete");
     const id = req.params.id;
     Booking.findByIdAndDelete(id)
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  uname: {
+    type: String,
+    required: [true, 'Please enter an username'],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
+});
+
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// static method to login user
+userSchema.statics.login = async function(uname, password) {
+  const user = await this.findOne({ uname }).catch(err=>{console.log(err)});
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect username');
+};
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
